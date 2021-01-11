56269fea0d983dd1d3ecf850b2859ebfc8148eef
= <=>
2021-01-11 17:13:03 +0200

    Update questions

--- resources/views/layouts/_messages.blade.php
+++ resources/views/layouts/_messages.blade.php
@@ -1,8 +1,19 @@
 @if(session('success'))
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Success!</strong> {{session('success')}}.
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
   </div>
+@endif
+
+
+@if(session('update'))
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> {{session('update')}}
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
 @endif
