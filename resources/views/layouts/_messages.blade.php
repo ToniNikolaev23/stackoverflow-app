cbe4ed4945184bae12c11e8d9bd7455987d86093
= <=>
2021-01-11 16:30:36 +0200

    change message style

--- resources/views/layouts/_messages.blade.php
+++ resources/views/layouts/_messages.blade.php
@@ -1,6 +1,8 @@
 @if(session('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
-    <strong>Success!</strong> {{session('success')}}
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
   </div>
 @endif
