1c2e1074717868b14bb63975197065277e913f17
= <=>
2021-01-12 17:33:05 +0200

    Implement Gate for Update and Delete question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -35,12 +35,15 @@
                             <div class="d-flex align-items-center">
                                 <h3 class="mt-0"><a href="{{ $question->url}}">{{ $question->title }}</a></h3>
                                 <div class="ml-auto">
+                                    @if(Auth::user()->can(['update-question', 'delete-question'], $question))
                                     <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    
                                     <form method="POST" class="form-delete" action="{{route("questions.destroy", $question->id)}}">
                                         @method('DELETE')
                                         @csrf
                                         <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Remove</button>
                                     </form>
+                                    @endif
                                 </div>
                             </div>
                             
